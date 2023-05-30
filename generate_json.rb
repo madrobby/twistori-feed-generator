68e537be30231ec61c60c559413f85f9f548eda4
Thomas Fuchs <thomas@slash7.com>
2023-05-29 18:55:38 -0700

    Generate Twistori database and ruby script to generate JSON data

---
+++ generate_json.rb
@@ -0,0 +1,28 @@
+require 'sqlite3'
+require 'json'
+
+# note: terms must be properly SQL escaped
+TERMS = %w(love hate think believe feel wish)
+STOPWORDS = %w(# http:// https:// nigger twistori twistory).map(&:downcase)
+
+TERMS.each do |item|
+  term = "i #{item}"
+  search = "%#{term.downcase}%"
+
+  db = SQLite3::Database.new("db.sqlite")
+  query = "SELECT text FROM POST WHERE TEXT LIKE \"#{search}\" AND length(TEXT)<=160 ORDER BY indexedAt DESC LIMIT 200"
+  puts query
+  hits = db.execute(query)
+
+  hits = hits.reject do |hit|
+    text = hit[0].downcase
+
+    STOPWORDS.any? { |word| text.include? word } ||
+    # reject if any of the other "i xxxx" combinations are included,
+    # this prevents duplicates between streams
+    (TERMS-[item]).any? { |word| text.include? "i #{word}" }
+  end
+
+  json = "data/#{term.gsub(' ','_')}.json"
+  File.open(json,'w'){|f| f.write hits.map{|t|t[0].gsub(/\s+/,' ').strip}.to_json }
+end
--- src/subscription.ts
+++ src/subscription.ts
@@ -5,22 +5,58 @@ import {
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
+  public postCount = 0
+  public lastSkeet = Date.now()
+
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
-
+    const STOPWORDS = /\b(musk|elon|muskrat|#[a-z0-9]+|bluesky|twistori|jay|paul|@[a-z0-9]|bsky)\b/gi
     // This logs the text of every post off the firehose.
     // Just for fun :)
     // Delete before actually using
     for (const post of ops.posts.creates) {
-      console.log(post.record.text)
+//      console.log("--> "+JSON.stringify(post.record))
+      // console.log(post.record.text)
     }
+
+    const now = Date.now()
+    const sps = (1000.0/(now - this.lastSkeet)) * ops.posts.creates.length
+    this.lastSkeet = now
 
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        const ltext = create.record.text.toLowerCase()
+
+        const match =
+          // todo: filter stopwords
+          // todo: filter hashtags (usually sports or nsfw)
+          // todo: filter longer entries?
+          create.record.facets === undefined &&
+          create.record.reply === undefined &&
+          (!ltext.match(STOPWORDS)) &&
+          (
+            ltext.match(/\bi\slove\b/) ||
+            ltext.match(/\bi\shate\b/) ||
+            ltext.match(/\bi\sthink\b/) ||
+            ltext.match(/\bi\sbelieve\b/) ||
+            ltext.match(/\bi\sfeel\b/) ||
+            ltext.match(/\bi\swish\b/)
+          )
+
+          // todo: for generating feed files
+          // take last 50 of every category and randomize
+          // update once an hour maybe?
+        // print console.log('\033[2J');
+        this.postCount = this.postCount + 1
+        process.stdout.write("\u001b[2K\u001b[0E --> post "+this.postCount+" "+sps+" skeets/s")
+
+        if (match) {
+          process.stdout.write('\n')
+          console.log(create.record.text)
+        }
+        return match
       })
       .map((create) => {
         // map alf-related posts to a db row
@@ -30,6 +66,7 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
           replyParent: create.record?.reply?.parent.uri ?? null,
           replyRoot: create.record?.reply?.root.uri ?? null,
           indexedAt: new Date().toISOString(),
+          text: create.record?.text
         }
       })
 
